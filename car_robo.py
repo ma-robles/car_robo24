07f00cb4fb79bc1d950dba152353ef57addfe812
Warangaranga <144378109+Warangaranga@users.noreply.github.com>
2023-09-19 14:19:00 -0600

    Update car_robo.py

--- car_robo.py
+++ car_robo.py
@@ -3,7 +3,12 @@ from time import sleep_ms
 #frente
 
 #reversa
-
+def rev (A1, A2, B1, B2, t)
+        A1.off()
+        B1.on()
+        A2.off()
+        B2.on()
+        sleep_ms(t)
 #alto
 
 #derecha
