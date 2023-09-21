43a6068a63c57aa8142063601b7df2576f19d3dd
Citlally-Galeana <72585409+Citlally-Galeana@users.noreply.github.com>
2023-09-21 12:24:59 -0600

    Add files via upload
    Pa la chetiza
    Esta es mi primera vez programando pero en git yo ya programaba

--- car_robo.py
+++ car_robo.py
@@ -9,27 +9,27 @@ def frente(A1, A2, B1, B2, t):
     sleep_ms(t)
 #reversa
 def rev (A1, A2, B1, B2, t):
-        A1.off()
-        B1.on()
-        A2.off()
-        B2.on()
-        sleep_ms(t)
+    A1.off()
+    A2.on()
+    B1.off()
+    B2.on()
+    sleep_ms(t)
 #alto
 def alto(A1, A2, B1, B2, t):
-  A1.off()
-  A2.off()
-  B1.off()
-  B2.off()
-  sleep_ms(t)
+    A1.off()
+    A2.off()
+    B1.off()
+    B2.off()
+    sleep_ms(t)
 #derecha
-def derecha (A1, A2, B1, B2, t):
-	A1.on()
-	B1.off()
-	A2.off()
-	B2.on()
-	sleep_ms(t)
-#izquierda
 def izquierda (A1, A2, B1, B2, t):
+    A1.on()
+    B1.off()
+    A2.off()
+    B2.on()
+    sleep_ms(t)
+#izquierda
+def derecho (A1, A2, B1, B2, t):
     A1.off()
     A2.on()
     B1.on()
--- main_car.py
+++ main_car.py
@@ -6,10 +6,10 @@ A2= Pin(35, Pin.OUT)
 B1= Pin(37, Pin.OUT)
 B2= Pin(39, Pin.OUT)
 
-t= 1000
+t= 3000
 
-car.frente(A1,A2,B1,B2,t)
-car.rev(A1,A2,B1,B2,t)
-car.alto(A1,A2,B1,B2,t)
-car.derecha(A1,A2,B1,B2,t)
+#car.frente(A1,A2,B1,B2,t)
+#car.rev(A1,A2,B1,B2,t)
+#car.derecha(A1,A2,B1,B2,t)
 car.izquierda(A1,A2,B1,B2,t)
+#car.alto(A1,A2,B1,B2,t)
